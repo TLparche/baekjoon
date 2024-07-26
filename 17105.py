8239af4792f3f02e448936131a5ce87db8b28c9b
tlparche <h010508@naver.com>
2024-07-26 22:30:51 +0900

    solve

---
+++ 17105.py
@@ -0,0 +1,99 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def power(a, b, mod):
+    result = 1
+    while b:
+        if b % 2:
+            result *= a
+            result %= mod
+        b //= 2
+        a *= a
+        a %= mod
+    return result
+
+
+def reverse_bit(a, n):
+    result = [0] * n
+    num_bits = n.bit_length() - 1
+    for i in range(n):
+        rev_i = 0
+        for j in range(num_bits):
+            if i & (1 << j):
+                rev_i |= 1 << (num_bits - 1 - j)
+        result[rev_i] = a[i]
+    return result
+
+
+def ntt(bit_arr, g, p, inv=False):
+    len_bit = len(bit_arr)
+    rev_bit_arr = reverse_bit(bit_arr, len_bit)
+
+    block_size = 2
+    while block_size <= len_bit:
+        w_len = power(g, p // block_size, p)
+        if inv:
+            w_len = power(w_len, p - 2, p)
+        for k in range(0, len_bit, block_size):
+            w = 1
+            for j in range(block_size // 2):
+                t = (w % p) * rev_bit_arr[k + j + block_size // 2]
+                u = rev_bit_arr[k + j]
+                rev_bit_arr[k + j] = (u + t) % p
+                rev_bit_arr[k + j + block_size // 2] = (u - t) % p
+                w *= w_len
+                w %= p
+        block_size *= 2
+
+    if inv:
+        temp = p - (p - 1) // len_bit
+        for i in range(len_bit):
+            rev_bit_arr[i] = (rev_bit_arr[i] * temp) % p
+    return rev_bit_arr
+
+
+g = 3
+p = 998244353
+
+max = 5 * 10 ** 5
+a = [0] + [1] * max
+b = [0] * (max + 1)
+div = 3
+while div ** 2 <= 10 ** 6:
+    if a[(div - 1) // 2]:
+        for i in range(div ** 2, 10 ** 6 + 1, 2 * div):
+            a[(i - 1) // 2] = 0
+    div += 2
+
+for i in range(max // 2 + 1):
+    if a[i]:
+        b[2 * i] = 1
+
+n_fft = 2 * len(a) - 1
+n_padded = 1 << n_fft.bit_length()
+a += [0] * (n_padded - len(a))
+b += [0] * (n_padded - len(b))
+
+a_ntt = ntt(a, g, p)
+b_ntt = ntt(b, g, p)
+b_ntt = [(a_ntt[i] * b_ntt[i]) % p for i in range(len(b_ntt))]
+a_ntt = [(a_ntt[i] * a_ntt[i]) % p for i in range(len(a_ntt))]
+a_ntt = ntt(a_ntt, g, p, True)[:max + 1]
+b_ntt = ntt(b_ntt, g, p, True)
+
+a_ntt += [0] * (n_padded - len(a_ntt))
+a_ntt = ntt(a_ntt, g, p)
+c_ntt = ntt(a, g, p)
+a_ntt = [(a_ntt[i] * c_ntt[i]) % p for i in range(len(a_ntt))]
+a_ntt = ntt(a_ntt, g, p, True)
+
+for _ in range(int(input())):
+    n = int(input())
+    count = 0
+    if a[(n - 4) // 2]:
+        count += 1
+    count += (a_ntt[n // 2 - 1] - 3 * b_ntt[n // 2 - 1] + 2 * a[n // 6]) // 6 + (b_ntt[n // 2 - 1] - a[n // 6]) + a[
+        n // 6]
+    print(count)
